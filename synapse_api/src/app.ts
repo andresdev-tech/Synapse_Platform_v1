dd0cfabc4b7b13b2dc3ea66ae4ee36ab73f3f92f
andresdev-tech <andrewjaramillo315@gmail.com>
2026-08-23 15:36:38 -0500

    integracion del rate limit de 5 intetos por IP durante 7 minutos by andres

--- synapse_api/src/app.ts
+++ synapse_api/src/app.ts
@@ -2,11 +2,16 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import rateLimit from "express-rate-limit";
 
 import router from "./routes/index";
 import { swaggerUi, swaggerSpec } from "./docs/swager";
 
 const app = express();
+const RatelimitGlobal = rateLimit({
+    windowMs: 7 * 60 * 1000,
+    limit: 5
+})
 
 app.use(helmet());
 app.use(express.json());
@@ -18,6 +23,7 @@ app.use(cors(
 ));
 app.use(express.json());
 app.use(morgan("combined"));
+app.use(RatelimitGlobal);
 
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
