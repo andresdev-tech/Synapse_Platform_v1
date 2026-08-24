1aaecf3ca112e3474279981d929fb45f5defa03d
jhon lenis <JALH33745051@soy.sena.edu.co>
2026-08-23 19:38:17 -0500

    correcion de codigo

--- synapse_api/src/modules/programas/programs.controller.ts
+++ synapse_api/src/modules/programas/programs.controller.ts
@@ -8,7 +8,7 @@ export class ProgramasController {
 
   static async asignarProfesor(req: Request, res: Response): Promise<any> {
       try {
-          const { id } = req.params; // programa_id
+          const id = String(req.params.id); // programa_id
           const { usuario_id } = req.body;
           
           if (!id || !usuario_id) {
@@ -46,7 +46,7 @@ export class ProgramasController {
 
     static async createSchedule(req: Request, res: Response): Promise<any> {
       try {
-          const programa_id = req.params.id;
+          const programa_id = String(req.params.id);
           const { modalidad, jornada, horarios_json } = req.body;
           
           if (!programa_id || !modalidad || !jornada) {
